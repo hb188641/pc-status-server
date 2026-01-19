b8b1e8475bcbf29fba9cc872bef932887638f712
hb188641 <hbyhby12321@gmail.com>
2026-01-19 21:19:25 +0900

    auto:

--- server.py
+++ server.py
@@ -22,25 +22,34 @@ def get_command():
     global command
     if command:
         cmd = command
-        return jsonify(command=cmd)
-    return jsonify(command=None)
+        return jsonify(command=cmd, online=(time.time() - last_ping < 20))
+    return jsonify(command=None, online=(time.time() - last_ping < 20))
 
 @app.route("/lock", methods=["POST"])
 def lock():
     global command
     pw = request.json.get("password")
     if pw != password:
-        return "잘못된 비밀번호"
+        return "0"
     command = "Locked"
-    return "잠금 명령 전송됨"
+    return "1"
 @app.route("/release", methods=["POST"])
 def release():
     global command
     pw = request.json.get("password")
     if pw != password:
-        return "잘못된 비밀번호"
+        return "0"
     command = "Released"
-    return "잠금 해제 명령 전송됨"
+    return "1"
+@app.route("/change-password", methods=["POST"])
+def set_password():
+    global password
+    pw = request.json.get("password")
+    new_pw = request.json.get("new_password")
+    if pw != password:
+        return "0"
+    password = new_pw
+    return "1"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
